756c051ab703e990bb1f213199b7942337848acb
johanna08 <johannaperez@JohannasMacBook.home>
2015-11-20 21:42:43 -0500

    submitting hw

--- javascript/app.js
+++ javascript/app.js
@@ -6,40 +6,41 @@ player1 = "rock"; // you can test your code by changing this to "paper" or "scis
 player2 = "paper"; // you can test your code by changing this to "rock" or "scissor"
 
 var winner;
+
 if ( player1 === "rock" && player2 === "paper" ) {
     winner = "player2";
 }
 // write all the other player1 and player2 possibilities here
 // OHTER IF STATEMENTS HERE
-var winner;
+
 if ( player1 === "rock" && player2 === "scissor" ) {
 winner = "player1";
 }
-var winner; 
+
 if ( player1 === "rock" && player2 === "rock" ) {
 winner = "tie";
 }
-var winner;
+
 if ( player1 === "paper" && player1 === "rock" ) {
 winner = "player1";
 } 
-var winner;
+
 if ( player1 === "paper" && player1 === "scissor" ) {
 winner = "player2";
 }
-var winner;
+
 if ( player1 === "paper" && player2 === "paper" ) {
 winner = "tie";
 } 
-var winner;
+
 if ( player1 === "scissor" && player2 === "rock" ) {
 winner = "player2";
 }
-var winner;
+
 if ( player1 === "scissor" && player2 === "paper" ) {
 winner = "player1";
 }
-var winner;
+
 if ( player1 === "scissor" && player2 === "scissor" ) {
 winner = "tie";
 }
