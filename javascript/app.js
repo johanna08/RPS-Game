03e5861648c3bcef652d04e6be39eec5fdbe43b1
johanna <johanna08perez@gmail.com>
2015-11-20 21:44:26 -0500

    Updating gh-pages from master branch via GitBot

---
+++ javascript/app.js
@@ -0,0 +1,54 @@
+var player1;
+var player2;
+
+// TO TEST, SET VALUES OF PLAYER HERE
+player1 = "rock"; // you can test your code by changing this to "paper" or "scissor"
+player2 = "paper"; // you can test your code by changing this to "rock" or "scissor"
+
+var winner;
+
+if ( player1 === "rock" && player2 === "paper" ) {
+    winner = "player2";
+}
+// write all the other player1 and player2 possibilities here
+// OHTER IF STATEMENTS HERE
+
+if ( player1 === "rock" && player2 === "scissor" ) {
+winner = "player1";
+}
+
+if ( player1 === "rock" && player2 === "rock" ) {
+winner = "tie";
+}
+
+if ( player1 === "paper" && player1 === "rock" ) {
+winner = "player1";
+} 
+
+if ( player1 === "paper" && player1 === "scissor" ) {
+winner = "player2";
+}
+
+if ( player1 === "paper" && player2 === "paper" ) {
+winner = "tie";
+} 
+
+if ( player1 === "scissor" && player2 === "rock" ) {
+winner = "player2";
+}
+
+if ( player1 === "scissor" && player2 === "paper" ) {
+winner = "player1";
+}
+
+if ( player1 === "scissor" && player2 === "scissor" ) {
+winner = "tie";
+}
+// END
+
+console.log( winner );
+
+// FOOD FOR THOUGHT:
+// how annoying is it that you have to go back and change your variables
+// to see different outcomes??
+// what could you do to the code to ensure that you could run our logic over and over?
